a79ac71023bcb5afb70759229a02dd6ffd9d1fbe
Mathewmanuel <mathewmanuel004@gmail.com>
2025-06-19 10:58:19 +0530

    Add files via upload

---
+++ overloading.py
@@ -0,0 +1,28 @@
+hi="peru"
+bro=34
+print(f"hi bro unga {hi}")
+print(f"vazhga valamudan{bro}")
+disc=0.10
+print(f"{disc*1000}% off")
+student=True
+print(f"{student}")
+isonline=False
+if isonline:
+    print("youo are smart")
+else:
+    print("you aint shit")
+name=""
+mark=25
+gpa=7.92
+isstdnt=True
+
+gpa=str(gpa)
+mark=str(mark)
+name=bool(name)
+print(mark)
+print(gpa)
+print(name)
+name=input("un per enna chellam \n")
+age=int(input("un vayasu enna\n"))
+print(f"{name} is my name ")
+print(f"{age} is my age ")
