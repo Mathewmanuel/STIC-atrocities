f95e321d351e8e6d5f0afc213a265ae8c6f8b35e
Mathewmanuel <mathewmanuel004@gmail.com>
2025-06-19 09:51:31 +0530

    Add files via upload
    shoppingcart

---
+++ bagboybadboy.py
@@ -0,0 +1,5 @@
+item=input("What would you like to buy")
+price=float(input("Enter the price of your stuff:\n"))
+quantity=int(input("enter the number of items you wanna buy:\n"))
+total=price*quantity
+print(f"neengal vaangiyavai {item}\n avatrin ennikkai{quantity}\n seitha motha selavu rubai{total}\n nandri meendum varuga \n ithu ungal chennai sillks ")
