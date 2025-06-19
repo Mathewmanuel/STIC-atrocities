6a45472ef66aeca70f356c6a77bf7576fbb7bf25
Mathewmanuel <mathewmanuel004@gmail.com>
2025-06-19 11:44:48 +0530

    Add files via upload

---
+++ recursino.py
@@ -0,0 +1,14 @@
+
+# def addone(num):
+#     if(num >= 9):
+#         return num+1
+#     total=num + 1
+#     print(total)
+#     return addone(total)
+# mynewtotal=addone(0)
+# print(mynewtotal)
+
+value=TRUE
+while value:
+    value=False
+    
