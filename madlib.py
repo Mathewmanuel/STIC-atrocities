58791b2d66c824c90632f4ffbcb674a9d0d889a4
Mathewmanuel <mathewmanuel004@gmail.com>
2025-06-19 10:09:58 +0530

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,7 @@
+name=input("Enter a name")
+place=input("enter a place")
+adjective=input("enter an adjective")
+verb=input("enter an verb")
+animal=input("enter an animal")
+
+print(f"{name} took me to{place}. we rode a {adjective} {animal}, it started {verb} after we left")
